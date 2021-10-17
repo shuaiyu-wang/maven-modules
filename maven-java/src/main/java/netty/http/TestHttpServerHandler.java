5fff7004cbd61dbb744f4041b5b87fe67b4593fa
shuaiyu-wang <1368905205@qq.com>
2021-10-17 16:57:09 +0800

    netty http 过滤资源

--- maven-java/src/main/java/netty/http/TestHttpServerHandler.java
+++ maven-java/src/main/java/netty/http/TestHttpServerHandler.java
@@ -7,6 +7,8 @@ import io.netty.channel.SimpleChannelInboundHandler;
 import io.netty.handler.codec.http.*;
 import io.netty.util.CharsetUtil;
 
+import java.net.URI;
+
 /*
 说明
 1. SimpleChannelInboundHandler 是ChannelInboundHandlerAdapter
@@ -20,8 +22,22 @@ public class TestHttpServerHandler extends SimpleChannelInboundHandler<HttpObjec
 
         // 判断 msg 是否是httpRequest 请求
         if (msg instanceof HttpRequest) {
+
+            System.out.println("pipeline hashcode = " + ctx.pipeline().hashCode() + "，TestHttpServerHandler hashcode" + this.hashCode());
+
+            // 获取httpRequest
+            HttpRequest httpRequest = (HttpRequest) msg;
+
             System.out.println("msg 类型=" + msg.getClass());
             System.out.println("客户端地址：" + ctx.channel().remoteAddress());
+
+
+            // 获取uri，过滤指定资源
+            URI uri = new URI(httpRequest.uri());
+            if ("/favicon.ico".equals(uri.getPath())) {
+                System.out.println("请求了 favicon.ico， 不做响应");
+                return;
+            }
 
             // 回复消息给浏览器 [ http协议]
             ByteBuf content = Unpooled.copiedBuffer("hello client, im 服务器", CharsetUtil.UTF_8);
